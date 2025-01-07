5d8d5bd24914235d629c3ebe8b1645063b6694c5
jaddy001 <jaddyjadnganga@gmail.com>
2025-01-07 08:58:46 +0000

    Fix canUnlockAll function and ensure correct outputs

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,19 +3,21 @@
 def canUnlockAll(boxes):
     # Set to keep track of unlocked boxes
     unlocked = set()
-    unlocked.add(0)  # The first box is unlocked
-
-    # List to use as a stack for DFS
+    unlocked.add(0)  # The first box is unlocked by default
+    
+    # Stack to perform DFS, starting from box 0
     stack = [0]
-
+    
+    # Perform DFS
     while stack:
-        current_box = stack.pop()
-
+        current_box = stack.pop()  # Get the current box
+        
+        # For every key in the current box
         for key in boxes[current_box]:
-            if key not in unlocked:
-                unlocked.add(key)
-                stack.append(key)
-
-    # Check if all boxes can be unlocked
+            if key not in unlocked and key < len(boxes):
+                unlocked.add(key)  # Unlock the box
+                stack.append(key)  # Add to stack to visit later
+    
+    # Check if all boxes have been unlocked
     return len(unlocked) == len(boxes)
 
