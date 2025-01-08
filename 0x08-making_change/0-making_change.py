961bae77bb25d85525006e5ff0016dfed669dc21
jaddy001 <jaddyjadnganga@gmail.com>
2025-01-08 14:09:31 +0000

    m

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""making change"""
+""" making change"""
 
 def makeChange(coins, total):
     """ return the minimal change """
     if total <= 0:
         return 0
 
-    coin.sort(reverse=True)
+    coins.sort(reverse=True)
     num_change = 0
     sum_change = 0
 
-    for i in coin:
+    for i in coins:
         while sum_change < total:
-            sum_change += 1
+            sum_change += i
             num_change += 1
         if sum_change == total:
             return num_change
-        sum_change -= 1
+        sum_change -= i
         num_change -= 1
 
     return -1
