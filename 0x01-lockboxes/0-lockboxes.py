60582524342e2c9275dc3c5adefdc0ec3ad34850
jaddy001 <jaddyjadnganga@gmail.com>
2025-01-13 02:26:47 -0800

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-    # Set to keep track of unlocked boxes
-    unlocked = set()
-    unlocked.add(0)  # The first box is unlocked by default
+    """
+    Determines if all the boxes can be unlocked.
     
-    # Stack to perform DFS, starting from box 0
+    The function performs a graph traversal (DFS) starting from box 0, using the keys 
+    in each unlocked box to open other boxes. The traversal continues until no more boxes 
+    can be unlocked. If all boxes are unlocked, the function returns True; otherwise, False.
+
+    Args:
+        boxes (list): A list of lists where each sublist represents a box, and contains 
+                      the keys (box numbers) that can be used to unlock other boxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    # Create a set to keep track of unlocked boxes, starting with box 0 unlocked
+    unlocked = set([0])
+    
+    # Initialize a stack to perform DFS. We start from box 0.
     stack = [0]
     
-    # Perform DFS
+    # Perform DFS to explore all reachable boxes
     while stack:
-        current_box = stack.pop()  # Get the current box
+        box = stack.pop()  # Take the last box from the stack
         
-        # For every key in the current box
-        for key in boxes[current_box]:
-            if key not in unlocked and key < len(boxes):
-                unlocked.add(key)  # Unlock the box
-                stack.append(key)  # Add to stack to visit later
+        # Check all the keys in the current box
+        for key in boxes[box]:
+            # If the key opens a box that has not been unlocked yet
+            if key < len(boxes) and key not in unlocked:
+                # Unlock the box and add it to the stack for further exploration
+                unlocked.add(key)
+                stack.append(key)
     
-    # Check if all boxes have been unlocked
+    # If the number of unlocked boxes equals the total number of boxes,
+    # it means all boxes can be unlocked.
     return len(unlocked) == len(boxes)
 
