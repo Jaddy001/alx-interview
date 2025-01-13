37f16426482ce13e14d3c26a56f21f28791f0194
jaddy001 <jaddyjadnganga@gmail.com>
2025-01-13 02:53:57 -0800

    changes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" a method that determines if all boxes can be opened """
+
+"""A method that determines if all boxes can be opened."""
+
 
 def canUnlockAll(boxes):
-    """ a method that determines if all boxes can be opened """
+    """A method that determines if all boxes can be opened."""
 
     unlocked = {0}
-
     stack = [0]
 
     while stack:
         current_box = stack.pop()
 
         for key in boxes[current_box]:
-            if key < len(boxes) and key is not in unlocked:
+            if key < len(boxes) and key not in unlocked:
                 unlocked.add(key)
                 stack.append(key)
+
     return len(unlocked) == len(boxes)
 
